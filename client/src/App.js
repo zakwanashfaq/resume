06ca1cf14d61b5dfe85883a4667ce6d20006c696
zakwanashfaqzian <zakwanashfaq@gmail.com>
2022-11-22 18:15:14 -0330

    backend talking to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [data, setData] = useState("Loading...");
+  fetch("/api")
+  .then((res) => res.json())
+  .then(json_data => {
+    setData(json_data.data.highlights[0])
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data}
         </p>
         <a
           className="App-link"
