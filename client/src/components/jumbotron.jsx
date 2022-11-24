750b839d72b477c31bb8744535fa432d3ee10c98
zakwanashfaqzian <zakwanashfaq@gmail.com>
2022-11-23 23:04:47 -0330

    modified jumbotron to spec

--- client/src/components/jumbotron.jsx
+++ client/src/components/jumbotron.jsx
@@ -4,10 +4,10 @@ import "./css/jumbotron.css";
 export const Jumbotron = (props) => {
     return (
         <div className="p-0 customJumbotron">
-            <div className="jumbotron mb-0 customJumbotron-container">
-                <div className="container">
-                    <h1 className="display-4">Hi, I am Zakwan</h1>
-                    <p className="lead"> Zakwan Ashfaq Zian</p>
+            <div className="jumbotron mb-0 customJumbotron-jumbotron">
+                <div className="container customJumbotron-container">
+                    <h1 className="display-4">Hi!</h1>
+                    <p className="lead">I am <a className="customJumbotron-name-highlight">Zakwan</a> Ashfaq Zian</p>
                     <div className="row pl-3">Memorial University of Newfoundland</div>
                     <div className="row pl-3">Bacherlor in Computer Science</div>
                     <div className="row pl-3">Software Engineer</div>
