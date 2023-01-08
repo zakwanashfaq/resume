43c3ad8976539799215ac816f6c65e05c2cd98ef
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2023-01-07 23:10:32 -0330

    image sizing fixed

--- client/pages/projects/item.jsx
+++ client/pages/projects/item.jsx
@@ -2,7 +2,7 @@ function Item(props) {
     const default_image = "https://www.rightclickit.com.au/wp-content/uploads/2018/09/IMAGE-COMING-SOON-1000.jpg";
     return <div className="item-container p-1 col-3">
         <div class="card">
-            <img src={props.image? props.image : default_image} alt="Unavailable" />
+            <img className="p-2" src={props.image? props.image : default_image} alt="Unavailable" />
             <div class="card-body">
                 <h5 class="card-title">{props.name}</h5>
                 <p class="card-text">{props.description}</p>
